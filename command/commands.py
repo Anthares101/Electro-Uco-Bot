92f14b2ef3ad4cdfeeeb724810a9d4d7427b2b5f
Angel Heredia <angel.angel802@gmail.com>
2018-05-08 16:54:18 +0200

    Repeite lo que se le manda si no coincide con algun comando

--- command/commands.py
+++ command/commands.py
@@ -9,7 +9,6 @@ from telebot import util
 def start(message):
     bot.reply_to(message, 'Buenas, ' + message.from_user.first_name)
 
-
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
-    bot.reply_voice(message, message.text)
+    bot.reply_to(message, message.text)
