dd5fdf57f83397ac9d210a576fcefbcf90859533
Tomás Espejo <i62esgot@uco.es>
2018-05-09 15:49:12 +0200

    Venga que podemos 15

--- command/commands.py
+++ command/commands.py
@@ -34,10 +34,10 @@ def start(message):
 def watson_bot(message):
 
 	almacenamiento = chat.Chat.get_config(message.chat.id, 'contexto')
-	#bot.reply_to(message, almacenamiento)
+	bot.reply_to(message, "%s" % almacenamiento)
 
 	contexto = almacenamiento.value
-	bot.reply_to(message, context.value)
+	bot.reply_to(message, "%s" % context.value)
 
 	response = assistant.message(
 	    workspace_id=WORKSPACE_ID,
