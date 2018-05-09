b80fbe8fd2ac1a228c5d63934ea4c31c742eae2f
Angel <angel.angel802@gmail.com>
2018-05-09 21:43:14 +0200

    Debug

--- command/commands.py
+++ command/commands.py
@@ -29,7 +29,7 @@ def ref(message):
     datos = json.loads(response2.read())
     total = 0
 
-    m=bot.send_message(message.chat.id, "Listado de productos:")
+    bot.send_message(message.chat.id, "Listado de productos:")
 
     for dato in datos:
         #edit_message_text(m.chat.id, m.message_id, m.text + "\n - " + dato["label"] + float(dato["total_ttc"]) + "\u20ac")
