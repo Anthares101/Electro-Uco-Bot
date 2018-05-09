f6a42b48713b84fff40c726b7ec543a5e6c018eb
Angel <angel.angel802@gmail.com>
2018-05-10 00:25:04 +0200

    Añadiendo total

--- command/commands.py
+++ command/commands.py
@@ -36,8 +36,8 @@ def ref(message):
         total = total + float(dato["total_ttc"])
 
     bot.send_message(message.chat.id, respuesta)
-    #repuesta2="Precio total: " + total + "\u20ac"
-    #bot.send_message(message.chat.id, respuesta2)
+    repuesta2="Precio total: " + str(total) + "\u20ac"
+    bot.send_message(message.chat.id, respuesta2)
     return
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
