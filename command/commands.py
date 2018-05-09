f599505e958dfcc8d73a3ec5d4204235f2e27d21
Angel <angel.angel802@gmail.com>
2018-05-09 21:52:47 +0200

    Debug

--- command/commands.py
+++ command/commands.py
@@ -32,7 +32,7 @@ def ref(message):
     bot.send_message(message.chat.id, "Listado de productos:")
 
     for dato in datos:
-        bot.send_message(message.chat.id, "\n - " + dato["label"] + float(dato["total_ttc"]) + "\u20ac")
+        bot.send_message(message.chat.id, "\n - " + dato["label"])# + float(dato["total_ttc"]) + "\u20ac")
         total = total + float(dato["total_ttc"])
 
     #respuesta=respuesta + "\nPrecio total: " + total + "\u20ac"
