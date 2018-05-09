f96e1ecbed694cc425591664f545f9f4e0be1b69
Tomás Espejo <i62esgot@uco.es>
2018-05-09 15:45:32 +0200

    Venga que podemos 14

--- command/commands.py
+++ command/commands.py
@@ -33,22 +33,24 @@ def start(message):
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def watson_bot(message):
 
-	contexto = chat.Chat.get_config(message.chat.id, 'contexto')
-	bot.reply_to(message, "%s" % context)
-	contexto = contexto.value
-	bot.reply_to(message, "%s" % context.value)
+	almacenamiento = chat.Chat.get_config(message.chat.id, 'contexto')
+	#bot.reply_to(message, almacenamiento)
+
+	contexto = almacenamiento.value
+	bot.reply_to(message, context.value)
+
 	response = assistant.message(
 	    workspace_id=WORKSPACE_ID,
 	    input={
 	        'text': message.text
 	    },
-	    context=chat.Chat.get_config(message.chat.id, 'contexto')
+	    context=contexto
 	)
 
-	bot.reply_to(message, chat.Chat.get_config(message.chat.id, 'contexto'))
+	#bot.reply_to(message, chat.Chat.get_config(message.chat.id, 'contexto'))
 
 	contexto = json.dumps(response['context'])
 	chat.Chat.set_config(message.chat.id, 'contexto', contexto)
 
 	bot.reply_to(message, response['output']['text'][0])
-	bot.reply_to(message, contexto)
+	#bot.reply_to(message, contexto)
