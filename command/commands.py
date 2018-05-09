b603d240bf5e26f6e5c0d34cb33b10be0497a0b9
Angel Heredia <angel.angel802@gmail.com>
2018-05-09 13:42:19 +0200

    Debug

--- command/commands.py
+++ command/commands.py
@@ -11,14 +11,14 @@ def start(message):
 	bot.reply_to(message, 'Buenas ' + message.from_user.first_name+', introduzca una referencia de un pedido para ver información relativa a ese pedido')
 	#bot.register_next_step_handler(message, ref)
 
-def ref(message):
+"""def ref(message):
 	referencia = message.text
 
 	url = "https://www.ucotest.es/panel/webservice/consultabot.php?case=order&userID=9&ref="+referencia
 	response = urllib.urlopen(url)
 	data = json.loads(response.read())
 	for datos in data:
-		print datos['rowid']
+		print datos['rowid']"""
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
