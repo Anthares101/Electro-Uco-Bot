4f68ebf5c1bc40fb535ecc6dabfc63f45db37012
Angel Heredia <angel.angel802@gmail.com>
2018-05-09 14:57:16 +0200

    Debug

--- command/commands.py
+++ command/commands.py
@@ -7,17 +7,17 @@ from model import chat
 
 @bot.message_handler(commands=['start'])
 def start(message):
-	bot.sendMessage(message.chat.id, 'Buenas' + message.from_user.first_name + ', introduzca una referencia de un pedido para ver información relativa a ese pedido')
-	#bot.register_next_step_handler(message, ref)
+	bot.sendMessage(message.chat.id, 'Buenas, introduzca una referencia de un pedido para ver información relativa a ese pedido')
+	bot.register_next_step_handler(message, ref)
 
-"""def ref(message):
+def ref(message):
 	referencia = message.text
 
 	url = "https://www.ucotest.es/panel/webservice/consultabot.php?case=order&userID=9&ref="+referencia
 	response = urllib.urlopen(url)
 	data = json.loads(response.read())
 	for datos in data:
-		print datos['rowid']"""
+		print datos['rowid']
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
