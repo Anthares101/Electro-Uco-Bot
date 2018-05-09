4c5eeffc0d2ab44d200b3823de225847b3d54b7a
Angel Heredia <angel.angel802@gmail.com>
2018-05-09 13:59:03 +0200

    Debug

--- command/commands.py
+++ command/commands.py
@@ -7,8 +7,7 @@ from model import chat
 
 @bot.message_handler(commands=['start'])
 def start(message):
-	bot.reply_to(message, 'Buenas introduzca una referencia de un pedido para ver información relativa a ese pedido')
-	bot.reply_to(message, message.text)
+	bot.reply_to(message, 'Buenas'+message.from.first_name+', introduzca una referencia de un pedido para ver información relativa a ese pedido')
 	#bot.register_next_step_handler(message, ref)
 
 """def ref(message):
