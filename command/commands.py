f28548c871af299ff8619f8351cd62a08e535674
Angel Heredia <angel.angel802@gmail.com>
2018-05-09 09:44:43 +0200

    Pruebas en comando start

--- command/commands.py
+++ command/commands.py
@@ -7,7 +7,11 @@ from telebot import util
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message, 'Buenas, ' + message.from_user.first_name+', introduce su id de usuario')
+	if chat.Chat.get_config(chat_id, 'checkStart') != 1:
+		bot.reply_to(message, 'Buenas ' + message.from_user.first_name+', usa el comando /id para introducir su id de usuario')
+		chat.Chat.set_config(chat_id, 'checkStart', 1)
+def id(message):
+	data = util.extract_arguments(message.text)
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
 def echo_message(message):
