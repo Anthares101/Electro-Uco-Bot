071e74a9c0b199cc48d299ca837780ddfd0fe607
Ángel <33127189+anthares101@users.noreply.github.com>
2018-05-10 14:26:29 +0200

    Implementación de IBM Watson implementada
    Se ha realizado un merge de la rama watson en master para disponer así de la integración de IBM Watson en el bot.

--- command/commands.py
+++ command/commands.py
@@ -2,14 +2,27 @@
 from __future__ import unicode_literals
 import requests
 import os
-import urllib, json
-from heroku import bot
+from heroku import bot, USERNAME, PASSWORD, WORKSPACE_ID
 from telebot import util
+import urllib, json
+import watson_developer_cloud
 from model import chat
+
+assistant = watson_developer_cloud.AssistantV1(
+    username=USERNAME,
+    password=PASSWORD,
+    version='2018-02-16'
+)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Buenas " + message.from_user.first_name + ", introduzca /ref para realizar la consulta de un pedido")
+
+	response = assistant.message(
+	    workspace_id=WORKSPACE_ID
+	)
+
+	contexto = json.dumps(response['context'])
+	chat.Chat.set_config(message.chat.id, 'contexto', contexto)
 
 @bot.message_handler(commands=['ref'])
 def ref(message):
@@ -42,5 +55,19 @@ def reference(message):
     return
 
 @bot.message_handler(func=lambda message: True, content_types=['text'])
-def echo_message(message):
-    bot.reply_to(message, message.text)
+def watson_bot(message):
+
+	contexto = chat.Chat.get_config(message.chat.id, 'contexto')
+
+	response = assistant.message(
+	    workspace_id=WORKSPACE_ID,
+	    input={
+	        'text': message.text
+	    },
+	    context=json.loads(contexto.value)
+	)
+
+	contexto = json.dumps(response['context'])
+	chat.Chat.set_config(message.chat.id, 'contexto', contexto)
+
+	bot.send_message(message.chat.id, response['output']['text'][0])
