c74e6032a356b9cb7124d824c2162a1b5ca356bb
Angel <angel.angel802@gmail.com>
2018-05-10 17:32:23 +0200

    Cambio en el formato de impresion

--- command/commands.py
+++ command/commands.py
@@ -44,12 +44,14 @@ def reference(message):
     datos = json.loads(response.read())
 
     for dato in datos:
-        respuesta="ID del pedido: " + dato["rowid"] + "\nCodigo de referencia del pedido: " + dato["ref"] + "\nFecha del pedido: " + dato["date_commande"]
+        respuesta=("ID del pedido: " + dato["rowid"] + "\nCodigo de referencia del pedido: " + dato["ref"] + "\nFecha del pedido: " + dato["date_commande"])
 
+    bot.send_message(message.chat.id, respuesta)
+    
     datos = json.loads(response2.read())
     total = 0
 
-    respuesta=respuesta+"\n\n\nListado de productos:\n\n"
+    respuesta="Listado de productos:\n\n"
 
     for dato in datos:
         total_ttc=float(dato["total_ttc"])
