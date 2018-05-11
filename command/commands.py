9404b2961c4c5897a9b9155bbed6cf8fb95b221a
Angel <angel.angel802@gmail.com>
2018-05-11 17:18:09 +0200

    Añadidos emojis

--- command/commands.py
+++ command/commands.py
@@ -61,26 +61,25 @@ def ref(message):
     else:
 
         for dato in datos:
-            respuesta=str("*Codigo de referencia del pedido:* " + dato["ref"] + "\n*Fecha del pedido:* " + dato["date_commande"])
+            respuesta=str("📝 *Código de referencia del pedido:* " + dato["ref"] + "\n📆 *Fecha del pedido:* " + dato["date_commande"])
         
         total = 0
 
-        respuesta=respuesta+"\n\n\n*Listado de productos:*\n\n"
+        respuesta=respuesta+"\n\n\n📋 *Listado de productos:*\n\n"
 
         for dato in datos2:
             total_ttc=float(dato["total_ttc"])
             respuesta=respuesta + "- " + "_" + dato["label"] + "_" + "\t\t" + "_" + str(total_ttc) + "_" + "\u20ac\n"
             total = total + float(dato["total_ttc"])
 
-        respuesta=(respuesta + "\n\n*Precio total:* " + str(total) + "\u20ac")
+        respuesta=(respuesta + "\n\n💶 *Precio total:* " + str(total) + "\u20ac")
         
         estados = { 0:"_Borrador_", 1:"_En curso_", 2:"_Entregado_" }
 
         for dato in datos3:
-            respuesta=respuesta + "\n\n*Estado del pedido:* " + estados[int(dato["fk_statut"])]
+            respuesta=respuesta + "\n\n🚚 *Estado del pedido:* " + estados[int(dato["fk_statut"])]
         
         bot.send_message(message.chat.id, respuesta, parse_mode="Markdown")
-	bot.send_message(message.chat.id, "😀",  parse_mode="Markdown")
 
     return
 
