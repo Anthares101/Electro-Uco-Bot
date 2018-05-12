60665c1b7d4031cc5ebf8debca3d2ed662ac7232
Unknown <i62gomos@uco.es>
2018-05-12 14:25:07 +0200

    DEBUG

--- command/commands.py
+++ command/commands.py
@@ -45,7 +45,7 @@ def ls(message):
 
 		datos2 = json.loads(response2.read())
 
-		bot.send_photo(message.chat.id, datos2)
+		bot.send_photo(message.chat.id, datos2, caption="hola")
 
 @bot.message_handler(commands=['ref'])
 def ref(message):
