cbe6008384b1d85e088ed20d313707b9562274c8
Tomás Espejo <i62esgot@uco.es>
2018-05-13 19:02:25 +0200

    Debuging definitivo (espero 3)

--- command/commands.py
+++ command/commands.py
@@ -56,11 +56,6 @@ def list(message):
         bot.send_message(message.chat.id, "Ha habido un error al realizar su consulta de pedido")
 
     else:
- """       context = chat.Chat.get_config(message.chat.id, 'contexto')
-        context = json.loads(context.value)
-        context['hay_pedido'] = "true"
-        context = json.dumps(context)
-        chat.Chat.set_config(message.chat.id, 'contexto', context)"""
 
 	chat.Chat.set_config(message.chat.id, 'referencia', referencia)
 
@@ -132,11 +127,6 @@ def info(message):
         bot.send_message(message.chat.id, "Ha habido un error al realizar su consulta de pedido")
 
     else:
-"""        context = chat.Chat.get_config(message.chat.id, 'contexto')
-        context = json.loads(context.value)
-        context['hay_pedido'] = "true"
-        context = json.dumps(context)
-        chat.Chat.set_config(message.chat.id, 'contexto', context)"""
 
 	chat.Chat.set_config(message.chat.id, 'referencia', referencia)
 
