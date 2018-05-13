721de656e58077d6af794b62de0e086ada8ac814
Unknown <i62gomos@uco.es>
2018-05-13 11:32:57 +0200

    Añadido link a la página (we hope)

--- command/commands.py
+++ command/commands.py
@@ -53,28 +53,31 @@ def list(message):
     else:
         for dato in datos:
             url2 = "https://www.ucotest.es/panel/webservice/consultabot.php?case=getImage&ref=" + dato['ref']
+            url3 = "https://www.ucotest.es/panel/webservice/consultabot.php?case=urlshop&ref=" + dato['ref']
 
             response2 = urllib.urlopen(url2)
+            response3 = urllib.urlopen(url3)
 
             datos2 = json.loads(response2.read())
+            datos3 = json.loads(response3.read())
 
-            if (datos2 == 1):
+            if (datos2 == 1 or datos3 == 1):
                 bot.send_message(message.chat.id, "Ha habido un error al realizar su consulta de pedido")
 
-            elif (datos2 == 2):
+            elif (datos2 == 2 or datos3 == 2):
                 bot.send_message(message.chat.id, "No se ha podido localizar su pedido")
 
-            elif (datos2 == 3):
+            elif (datos2 == 3 or datos3 == 3):
                 bot.send_message(message.chat.id, "Ha habido un error al realizar su consulta de pedido")
 
-            elif (datos2 == 4):
+            elif (datos2 == 4 or datos3 == 4):
                 bot.send_message(message.chat.id, "Ha habido un error al realizar su consulta de pedido")
 
             else:
                 nombre = dato["label"]
                 precio = float(dato["total_ttc"])
 
-                bot.send_photo(message.chat.id, datos2, caption="🛒 _" + nombre + "_" + "\n💶 *Precio:* " + str(precio) + "\u20ac", parse_mode="Markdown")
+                bot.send_photo(message.chat.id, datos2, caption="🛒 _" + nombre + "_" + "\n💶 *Precio:* " + str(precio) + "\u20ac\n\n" + datos3, parse_mode="Markdown")
 
     return
 
